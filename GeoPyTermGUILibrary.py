0009186ff0c8e3c249ca0fbe58692d21b6c57d09
Geode42 <99060684+Geode42@users.noreply.github.com>
2022-03-20 22:38:10 -0400

    Add prototype version of fprint

--- GeoPyTermGUILibrary.py
+++ GeoPyTermGUILibrary.py
@@ -1,25 +1,27 @@
 from colorsys import hsv_to_rgb
 
+show_warnings = True
+
 class __formatting:
 	def __init__(self):
 		self.__dictionary = {
-			'reset': '\33[0m',
-			'bold': '\33[1m',
-			'light': '\33[2m',
-			'italic': '\33[3m',
-			'underline': '\33[4m',
+			'reset': '\33[0m', 'r': '\33[0m',
+			'bold': '\33[1m', 'b': '\33[1m',
+			'light': '\33[2m', 'l': '\33[2m',
+			'italic': '\33[3m', 'i': '\33[3m',
+			'underline': '\33[4m', 'u': '\33[4m',
 			'blink': '\33[5m',
 			'invert': '\33[7m',
-			'hide': '\33[8m',
-			'strikethrough': '\33[9m',
+			'hide': '\33[8m', 'h': '\33[8m',
+			'strikethrough': '\33[9m', 's': '\33[9m',
 			'double underline': '\33[21m',
-			'reset intensity': '\33[22m',
-			'reset italic': '\33[23m',
-			'reset underline': '\33[24m',
+			'reset intensity': '\33[22m', 'rb': '\33[22m', 'rl': '\33[22m',
+			'reset italic': '\33[23m', 'ri': '\33[23m',
+			'reset underline': '\33[24m', 'ru': '\33[24m',
 			'reset blink': '\33[25m',
 			'reset invert': '\33[27m',
 			'reset hide': '\33[28m',
-			'reset strikethrough': '\33[29m',
+			'reset strikethrough': '\33[29m', 'rs': '\33[29m',
 			'dblack': '\33[30m',
 			'dred': '\33[31m',
 			'dgreen': '\33[32m',
@@ -29,7 +31,7 @@ class __formatting:
 			'dcyan': '\33[36m',
 			'dwhite': '\33[37m',
 			# '': '\33[38m',  # implemented via function
-			'reset color': '\33[39m',
+			'reset color': '\33[39m', 'rc': '\33[39m',
 			'background dblack': '\33[m40',
 			'background dred': '\33[m41',
 			'background dgreen': '\33[m42',
@@ -39,12 +41,9 @@ class __formatting:
 			'background dcyan': '\33[m46',
 			'background dwhite': '\33[m47',
 			# '': '\33[m48',  # implemented via function
-			'reset background': '\33[m49',
+			'reset background': '\33[m49', 'rbg': '\33[49m',
 			# '': '\33[m58',  # implemented via function
-			'reset underline color': '\33[m59',
-			'superscript': '\33[m73',
-			'subscript': '\33[74m',
-			'reset script': '\33[75m',
+			'reset underline color': '\33[m59', 'ruc': '\33[59m',
 			'black': '\33[90m',
 			'red': '\33[91m',
 			'green': '\33[92m',
@@ -68,6 +67,15 @@ class __formatting:
 	
 	def __getitem__(self, key):
 		return self.__dictionary[key]
+	
+	def __contains__(self, item):
+		if item in self.__dictionary:
+			return True
+		return False
+	
+	def __iter__(self):
+		for key, value in self.__dictionary.items():
+			return key
 
 f = __formatting()
 
@@ -95,3 +103,97 @@ def underline_from_hsv(h, s, v):
 	r, g, b = hsv_to_rgb(h, s, v)
 	r, g, b = round(r * 255), round(g * 255), round(b * 255)
 	return f'\033[58;2;{r};{g};{b}m'
+
+def fprint(text, start='', end=''):
+	text = start + text + end
+
+	d = {
+		'b': f.bold,
+		'i': f.italic,
+		'u': f.underline,
+		'l': f.light,
+		's': f.strikethrough,
+		'blink': f.blink,
+		'double underline': f.double_underline,
+		'red': f.red,
+		'green': f.green,
+		'yellow': f.yellow,
+		'blue': f.blue,
+		'purple': f.purple,
+		'cyan': f.cyan,
+	}
+
+	active_elements = []
+	custom_rgb = False
+
+	ineq_text = None
+	ending = False
+	for i in text:
+		if ineq_text is not None and i == '>':
+			if ineq_text.startswith('rgb'):
+				if ending:
+					if custom_rgb:
+						for j in active_elements:
+							if '\x1b[' in j:
+								active_elements.remove(j)
+								custom_rgb = False
+								print(f.r, end='')
+								for j in active_elements:
+									print(j, end='')
+								ineq_text = None
+								ending = False
+								break
+					else:
+						if show_warnings:
+							print(f.r + f.red + f'|===== WARNING: Tag {ineq_text} not active =====|' + f.r)
+				else:
+					color = []
+					ineq_text = ineq_text[3:]
+					if ineq_text.startswith('('):
+						ineq_text = ineq_text[1:]
+						if ineq_text.endswith(')'):
+							ineq_text = ineq_text[:-1]
+							ineq_text = ineq_text.split(',')
+							if len(ineq_text) == 3:
+								for val in ineq_text:
+									val = val.strip(' ')
+									if val.isnumeric:
+										color.append(int(val))
+								active_elements.append(color_from_rgb(color[0], color[1], color[2]))
+								custom_rgb = True
+								print(f.r, end='')
+								for j in active_elements:
+									print(j, end='')
+								ineq_text = None
+								ending = False
+			elif ineq_text in d:
+				if ending:
+					if d[ineq_text] in active_elements:
+						active_elements.remove(d[ineq_text])
+					else:
+						if show_warnings:
+							print(f.r + f.red + f'|===== WARNING: Tag {ineq_text} not active =====|' + f.r)
+				else:						
+					active_elements.append(d[ineq_text])
+
+				print(f.r, end='')
+				for j in active_elements:
+					print(j, end='')
+				ineq_text = None
+				ending = False
+			else:
+				if show_warnings:
+					print(f.r + f.red + f'|===== WARNING: Tag {ineq_text} not found =====|' + f.r)
+
+					for j in active_elements:
+						print(j, end='')
+		elif ineq_text is not None:
+			if len(ineq_text) == 0 and i == '/':
+				ending = True
+			else:
+				ineq_text += i
+		elif i == '<':
+			ineq_text = ''
+		else:
+			print(i, end='')
+	print(end)
